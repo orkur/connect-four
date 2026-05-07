5665a35edb7a7948a67070e35f486b67e76dda70
orkur <orkur123@gmail.com>
2026-05-07 11:11:40 +0200

    implement game state

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -19,3 +19,32 @@ data class GameConfig (
         require(winLength <= maxOf(rows, columns)) { "Win length must be <= maxOf(rows, columns)" }
     }
 }
+
+
+data class GameState(
+    val config: GameConfig = GameConfig(),
+    val board: List<List<Player?>> = List(config.rows) { List(config.columns) { null } },
+    val currentPlayer: Player = Player.Red
+)
+
+fun GameState.dropPiece(column: Int): GameState {
+    if (column < 0 || column >= board.size) error("Column must be in [0, ${board.size-1}]")
+
+    val rowNumber = (config.rows - 1 downTo 0).firstOrNull { row -> board[row][column] == null } ?: error("full row")
+
+    val newBoard = board.mapIndexed { i, row ->
+        if (i == rowNumber)
+            row.mapIndexed { j, cell ->
+                if (j == column) currentPlayer else cell
+            }
+        else
+            row
+    }
+    //place for winning condition
+
+     return copy(
+         board = newBoard,
+         currentPlayer = currentPlayer.next()
+     )
+}
+
